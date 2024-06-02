7d7c5c017324762796c6f0e07957772d16b2a872
Aryan Singh <166108845+DevAryanSin@users.noreply.github.com>
2024-06-02 21:28:11 +0530

    Create helpiamstuck.cpp

---
+++ strings/helpiamstuck.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+string name;
+int main(){
+    
+    while(1==1){
+      cout << "Help! I'm stuck in an infinite loop ";}
+    return 0;
+}
