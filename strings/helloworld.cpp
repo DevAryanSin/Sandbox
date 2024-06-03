1f15cf6363657bf8aa64ff374d05eed95ea326ba
Aryan Singh <166108845+DevAryanSin@users.noreply.github.com>
2024-06-03 23:09:48 +0530

    Create helloworld.cpp

---
+++ strings/helloworld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// This is single line comment
+/*This is a multi line comment
+this is a line
+MULTI LINE
+*/
+
+int main (){
+     cout<<"Hello World, I like pizza "  ; 
+     cout<<"It's really good" ; 
+    return 0;
+
+}
