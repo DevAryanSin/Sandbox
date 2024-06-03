ccb4f10bdfac542d35b985a34b5dd25772fab2f8
Aryan Singh <166108845+DevAryanSin@users.noreply.github.com>
2024-06-03 23:10:15 +0530

    Create var.cpp

---
+++ strings/var.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int x = 67;
+    int y = 51;
+    int sum = x + y ;
+
+    cout<<"The sum of x and y is" + sum;
+    return 0;
+
+
+
+}
