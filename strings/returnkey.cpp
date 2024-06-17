fdc7cbb085ff51fb5baf18fdc5a326b74a6d4d07
Aryan Singh <166108845+DevAryanSin@users.noreply.github.com>
2024-06-17 19:09:32 +0530

    Create returnkey.cpp

---
+++ strings/returnkey.cpp
@@ -0,0 +1,55 @@
+// return- return a value back to the spot where you called the function
+// return a value back
+// *************************************************************************************************************************
+// RETURN- kisi aur function mei calculate krke main function mei uski value as a datatype daalna
+// idhar square function mei hum result as integer calculate kr rahe hain    FIR   us result ko hum main function mei area ke
+// andar substitute kr rahe hai as an integer
+// value return krna
+// jo value calculate hui vo print ke bajaye code mei hi return ho rahi hai aur final output mil raha hai
+// ************************************************************************************************************************
+
+#include <iostream>
+
+using namespace std;
+
+double square(double length);
+
+int main(){
+
+    double length = 5.0;
+    double area = square(length);
+    cout<<"Area of the square is: "<<area<< "cm^2"<< endl;
+    return 0;
+}
+
+double square(double length){ // we are returning a double so we change void into a double
+    double result = length*length;
+    return result;
+    // or
+    // return length*length
+
+}
+// ************************************************************************************************************************
+
+// OR
+// wihtout using reurn direct cout and void
+// #include <iostream>
+
+// using namespace std;
+
+// void square(){
+
+
+//     double length;
+//     cout<< "Enter the length of the square:";
+//     cin >> length;
+//     double area  = length*length;
+//     cout<<"Area of the square is:"<< area << "cm^2";
+// }
+
+// int main(){
+//     square();
+//     return 0;
+
+// }
+// ***********************************************************************************************************************
